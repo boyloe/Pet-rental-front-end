9b5dd0bb849842b277c9b0720e332e260c8bd380
TJBachorz <TJBachorz@gmail.com>
2020-09-10 11:39:30 -0600

    filters species, but doesn't hold user ID

--- pets/pets.js
+++ pets/pets.js
@@ -1,7 +1,15 @@
-const queryParams = new URLSearchParams(window.location.search)
-const userId = queryParams.get('user_id')
+const userQueryParams = new URLSearchParams(window.location.search)
+const userId = userQueryParams.get('user_id')
 
-fetch("http://localhost:3000/pets")
+const speciesQueryParams = new URLSearchParams(window.location.search)
+const species = speciesQueryParams.get("species")
+
+let baseURL = "http://localhost:3000/pets"
+
+if (species) {
+    baseURL = `${baseURL}?species=${species}`
+}
+fetch(`${baseURL}`)
     .then(response => response.json())
     .then(pets => displayPetCard(pets))
 
