c9ac1b6c83d0f0df3eaefac19d7c721b85f76331
TJBachorz <TJBachorz@gmail.com>
2020-09-24 10:55:47 -0600

    adds username and password fields on main pag

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const queryParams = new URLSearchParams(window.location.search)
 const userId = queryParams.get('user')
-const baseURL = "https://pet-renter-back-end.herokuapp.com/"
+const baseURL = "https://pet-renter-back-end.herokuapp.com"
 
-fetch(`${baseURL}users`)
+fetch(`${baseURL}/login`)
     .then(response => response.json())
-    .then(userSelection)
+    .then(console.log)
 
-function userSelection(users) {
-    const select = document.querySelector('#user-select')
-    users.forEach(user => {
-        const option = document.createElement('option')
-        option.value = user.id
-        option.textContent = user.name
-        select.appendChild(option)
-    })
-}
+
+
+// function userSelection(users) {
+//     const select = document.querySelector('#user-select')
+//     users.forEach(user => {
+//         const option = document.createElement('option')
+//         option.value = user.id
+//         option.textContent = user.name
+//         select.appendChild(option)
+//     })
+// }
